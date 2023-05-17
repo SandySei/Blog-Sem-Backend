c03139f8f5e7070310260386f703c4ddb0e6d3ec
Sandy <sandyalves8@hotmail.com>
2023-05-17 15:12:13 -0300

    Cria rotas para cada View nova

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
+import CreateView from '../views/Create.vue'
+import DetailView from '../views/Detail.vue'
+import EditView from '../views/Edit.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +12,22 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/create',
+      name: 'create',
+      component: CreateView
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: DetailView
+    },
+    {
+      path: '/edit/:id',
+      name: 'edit',
+      component: EditView
+    },
   ]
-})
+});
 
-export default router
+export default router;
