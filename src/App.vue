68e62eb6bc5b507ea5b53e0930b214672fdcb069
Sandy <sandyalves8@hotmail.com>
2023-05-22 15:17:49 -0300

    Css

--- src/App.vue
+++ src/App.vue
@@ -55,56 +55,9 @@ export default {
 </template>
 
 <style scoped>
-header {
-  background: linear-gradient(
-    360deg,
-    rgba(255, 255, 255, 1) 0%,
-    rgba(0, 0, 0, 0.7) 100%
-  );
 
-  position: absolute;
-  width: 100vw;
-
-  top: 0;
-  left: 0;
-}
-a {
-  color: #000000;
-  text-decoration: none;
-  font-weight: bolder;
-  letter-spacing: 0.5px;
-
-  width: 50%;
-  height: 40px;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  position: relative;
-  z-index: 0;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  -webkit-transition: all 0.5s ease;
-  transition: all 0.5s ease;
-}
-
-a:hover {
-  color: #0018b3;
-  -webkit-transform: scale(1.2);
-  transform: scale(1.2);
-  letter-spacing: 1px;
-}
-
-nav {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-}
 
 main {
-  padding-top: 90px;
+  padding-top: 100px;
 }
 </style>
